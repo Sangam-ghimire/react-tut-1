e80fe0ae7aea4634e655e4f8156840264b00dce6
Sangam Ghimire <1sangamghimire1@gmail.com>
2022-10-12 00:29:20 +0545

    about us completed

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,50 +1,76 @@
 import React from "react";
-import team1 from "../assets/team1.png";
-import team2 from "../assets/team2.png";
-import team3 from "../assets/team3.png";
-// import "./Aboutus.css";
+
+import "./Aboutus.css";
+import Card from "../components/Card";
 
 const AboutUs = () => {
   return (
     <div>
-      <div id="details"> </div>
-      <div id="team" >
-      <div id="socialmedia">
+      <div id="Nirvani">
+        <div id="imagegroup">
+          <div id="firstimgcol">
+            <div id="topl">
+              <img src="top right.png" alt="error" />
+            </div>
+            <div id="buttonl">
+              <img src="botton left.png" alt="error" />
+            </div>
+          </div>
+
+          <div id="secondimgcol">
+            <div id="topr">
+              <img src="toprightt.png" alt="error" />
+            </div>
+            <div id="buttonr">
+              <img src="bottonright.png" alt="error" />
+            </div>
+          </div>
+        </div>
+        <div id="thirdtextcol">
+          <div id="title"> Nirvani Garden</div>
+          <div id="desc">
+            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            vulputate libero et velit interdum, ac aliquet odio mattis. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos. Curabitur tempus urna at turpis condimentum
+            lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis
+            condimentum ac, vestibulum eu nisl.
+          </div>
+          <div id="buttons">
+            <button id="button1">Find us</button>
+            <button id="button2">Contact us</button>
+          </div>
+        </div>
+      </div>
+
+      <div id="team">
         <div id="heading">
           <span id="headingi">Meet our Team</span>
         </div>
-        </div>
+
         <div id="products">
-          <div className="product">
-            <img src={team1} alt="sirste" />
-            <div id="plantte">Name</div>
-            <div id="planttee">Sub-heading</div>
-            <div id="plantteee">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
-              dolorum.
-            </div>
-            <div id="colorbar3"> </div>
-          </div>
-          <div className="product">
-            <img src={team2} alt="sirste" />
-            <div id="plantte">Name</div>
-            <div id="planttee">Sub-heading</div>
-            <div id="plantteee">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
-              dolorum.
-            </div>
-            <div id="colorbar3"> </div>
-          </div>
-          <div className="product">
-            <img src={team3} alt="sirste" />
-            <div id="plantte">Name</div>
-            <div id="planttee">Sub-heading</div>
-            <div id="plantteee">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
-              dolorum.
-            </div>
-            <div id="colorbar3"> </div>
-          </div>
+          <Card
+            imageurl="team1.png"
+            heading=" Name"
+            subheading="Sub heading"
+            text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, dolorum"
+            colorbar="colorbar"
+          />
+          <Card
+            imageurl="team2.png"
+            heading=" Name"
+            subheading="Sub heading"
+            text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, dolorum"
+            colorbar="colorbar2"
+          />
+          <Card
+            imageurl="team3.png"
+            heading=" Plant"
+            subheading="Sub heading"
+            text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, dolorum"
+            colorbar="colorbar3"
+          />
         </div>
       </div>
     </div>
